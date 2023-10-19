24e69e4339075a17b80afb91e96d9fffbe3a7418
hyunoi <nhe0622@gmail.com>
2023-10-19 16:13:58 +0900

    feat: project setting
    (cherry picked from commit 30f5cefc3a99bfb2ad8cdf5c8fd3c3d3b06b530d)

---
+++ src/main/java/gitME/gitMEApplication.java
@@ -0,0 +1,13 @@
+package gitME;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class gitMEApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(gitMEApplication.class, args);
+    }
+
+}
