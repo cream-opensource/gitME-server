9f1eeb8c59cb939aaff1fdef3168f1fbbd4ee2c2
m-a-king <9gle3492@gmail.com>
2023-11-13 23:06:18 +0900

    [타임리프 컨트롤러 오류 해결]

--- src/main/java/gitME/gitInterlink/controller/ThymeleafController.java
+++ src/main/java/gitME/gitInterlink/controller/ThymeleafController.java
@@ -7,13 +7,14 @@ import gitME.testANDtest.JpaTestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class ThymeleafController {
 
@@ -44,38 +45,4 @@ public class ThymeleafController {
         return "/gitInterlink"; // test
     }
 
-    @GetMapping("/user")
-    public ResponseEntity<List<User>> findUser() {
-        return ResponseEntity.ok(jpaTestService.findAllUser());
-    }
-
-    @GetMapping("/githubUser")
-    public ResponseEntity<List<GithubUser>> findGithubUser() {
-        return ResponseEntity.ok(jpaTestService.findAllGithubUser());
-    }
-
-    @PostMapping("/user")
-    public ResponseEntity<User> saveUser(@RequestBody User user) {
-        return ResponseEntity.ok(jpaTestService.saveUser(user));
-    }
-
-    @PostMapping("/githubUser")
-    public ResponseEntity<GithubUser> saveGithubUser(@RequestBody GithubUser githubUser) {
-        return ResponseEntity.ok(jpaTestService.saveGithubUser(githubUser));
-    }
-
-    @PostMapping("/repository")
-    public ResponseEntity<Repository> saveRepository(@RequestBody Repository repository) {
-        return ResponseEntity.ok(jpaTestService.saveRepository(repository));
-    }
-
-    @PostMapping("/codeStack")
-    public ResponseEntity<CodeStack> saveCodeStack(@RequestBody CodeStack codeStack) {
-        return ResponseEntity.ok(jpaTestService.saveCodeStack(codeStack));
-    }
-
-    @PostMapping("/externalLink")
-    public ResponseEntity<ExternalLink> saveExternalLink(@RequestBody ExternalLink externalLink) {
-        return ResponseEntity.ok(jpaTestService.saveExternalLink(externalLink));
-    }
 }
