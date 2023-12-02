00bf781a4d68d189798514eba5506d890aec4b1c
m-a-king <9gle3492@gmail.com>
2023-12-02 17:20:25 +0900

    [카카오 아이디로 userIdx 값 획득]

--- src/main/java/gitME/repository/UserRepository.java
+++ src/main/java/gitME/repository/UserRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByIdx(int idx);
+    Optional<User> findByIdx(int idx);
+
+    Optional<User> findByKakaoId(String kakaoId);
 }
